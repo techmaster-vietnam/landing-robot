ad5fc79757c84f706636ec1ad7ea1c2eaf07a690
buihien0109 <hien@techmaster>
2021-07-28 15:26:10 +0700

    update

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import "github.com/gofiber/fiber/v2"
+
+func main() {
+  app := fiber.New()
+
+
+  app.Static("/", "./dist") 
+
+  app.Get("/", func(c *fiber.Ctx) error {
+	return c.Render("index", fiber.Map{})
+  })
+
+  app.Listen(":8888")
+}

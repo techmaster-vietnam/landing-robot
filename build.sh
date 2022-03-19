3f827c268155c871ed3a266887140afe1b21c29e
japangermany1998 <nhatduc@techmaster.vn>
2022-03-19 14:56:13 +0700

    đức push

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
-docker build --no-cache -t registry.techmaster.vn/techmaster-game:latest .
+docker build --no-cache -t registry.techmaster.vn/techmaster-security:latest .
 
-docker push registry.techmaster.vn/techmaster-game:latest
+docker push registry.techmaster.vn/techmaster-security:latest
